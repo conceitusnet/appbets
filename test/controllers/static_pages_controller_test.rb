9ff975c9c09f337fc6f2259b9dd507c0f722cd16
conceitusnet <conceitusnet@yahoo.com>
2017-01-26 11:28:58 -0300

    Add full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,31 +2,26 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Bolão do brasileirão 2017"
-  end
-
-  #test "should get root" do
-  #  get FILL_IN
-  #  assert_response FILL_IN
+  #def setup
+  #  @base_title = "Bolão do brasileirão 2017"
   #end
 
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Bolão do brasileirão 2017"
   end
 
   test "should get ajuda" do
     get static_pages_ajuda_url
     assert_response :success
-    assert_select "title", "Ajuda | #{@base_title}"
+    assert_select "title", "Ajuda | Bolão do brasileirão 2017"
   end
 
   test "should get regras" do
     get static_pages_regras_url
     assert_response :success
-    assert_select "title", "Regras | #{@base_title}"
+    assert_select "title", "Regras | Bolão do brasileirão 2017"
   end
 
 end
